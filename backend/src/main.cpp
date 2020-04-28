5fb98936802ed68dae905d1c986c8c56a65cb115
edmbernard <bernard.erwan@gmail.com>
2020-04-28 10:31:04 +0200

    feat: add route list

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -42,13 +42,34 @@ int main(int argc, char **argv) {
 
     AsyncFileStreamer asyncFileStreamer(root);
 
-    uWS::App().get("/api/*", [&asyncFileStreamer](auto *res, auto *req) {
+    uWS::App()
+              .get("/api/register/:name", [&asyncFileStreamer](auto *res, auto *req) {
+                std::cout << req->getParameter(0) << std::endl;
+                res->writeHeader("Content-Type", "application/json")->end("{\"key\": \"XXX\", \"position\":{\"x\":1, \"y\": 1}}");
+              })
+              .post("/api/status/:key", [&asyncFileStreamer](auto *res, auto *req) {
+                std::cout << req->getParameter(0) << std::endl;
+                res->writeHeader("Content-Type", "application/json")->end("{\"position\":{\"x\":1, \"y\": 1}}");
+              })
+              .post("/api/move/:key/:x/:y", [&asyncFileStreamer](auto *res, auto *req) {
+                std::cout << req->getParameter(0) << std::endl;
+                std::cout << req->getParameter(1) << std::endl;
+                std::cout << req->getParameter(2) << std::endl;
+                res->writeHeader("Content-Type", "text/html; charset=utf-8")->end("success");
+              })
+              .post("/api/quit/:key", [&asyncFileStreamer](auto *res, auto *req) {
+                std::cout << req->getParameter(0) << std::endl;
+                res->writeHeader("Content-Type", "text/html; charset=utf-8")->end("success");
+              })
+              .get("/api/*", [&asyncFileStreamer](auto *res, auto *req) {
                 res->writeHeader("Content-Type", "application/json")->end("{\"hello\": \"world\"}");
               })
+              // Static File
               .get("/static/:folder/:file", [&asyncFileStreamer](auto *res, auto *req) {
                 asyncFileStreamer.streamFile(res, fmt::format("/{0}/{1}", req->getParameter(0), req->getParameter(1)));
                 res->end();
               })
+              // Default to home page
               .get("/*", [&asyncFileStreamer](auto *res, auto *req) {
                 // You can efficiently stream huge files too
                 asyncFileStreamer.streamFile(res, "/");
