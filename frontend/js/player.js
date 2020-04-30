fe595e01b4e6af5a35e9e0eebba6960e0f16c4fd
edmbernard <bernard.erwan@gmail.com>
2020-04-30 09:36:33 +0200

    feat: add some css

--- frontend/js/player.js
+++ frontend/js/player.js
@@ -29,10 +29,28 @@ var Player = {
       })
   },
   view: function (vnode) {
-    return m("player", [
-      m("h2", { class: "small-margin-bottom" }, "Player Index : " + PlayerState.playerIdx),
-      m("div", "Position : " + PlayerState.position.x + "," + PlayerState.position.y),
-      m("div", "Color : " + PlayerState.color),
+    return m("player.player-card.pure-g", [
+      m(".player-index.pure-u-1-3", [
+        m(".label", "Player Index"),
+        m(".value", PlayerState.playerIdx)
+      ]),
+      m(".player-position.pure-u-1-3", [
+        m(".label", "Position"),
+        m(".pure-g", [
+          m(".x-card.pure-u-1-2", [
+            m(".label", "x: "),
+            m(".value", PlayerState.position.x)
+          ]),
+          m(".y-card.pure-u-1-2", [
+            m(".label", "y: "),
+            m(".value", PlayerState.position.y)
+          ]),
+        ]),
+      ]),
+      m(".player-color.pure-u-1-3", [
+        m(".label", "Color"),
+        m(".value", PlayerState.color)
+      ])
     ]);
   }
 }
