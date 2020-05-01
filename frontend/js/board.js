21ecc9d27e375785d27958a07afcc91ddc81fd93
edmbernard <bernard.erwan@gmail.com>
2020-05-01 15:38:26 +0200

    fix: board creation without width height

--- frontend/js/board.js
+++ frontend/js/board.js
@@ -39,44 +39,46 @@ var Board = {
   oninit: function (vnode) {
     BoardState.size.width = vnode.attrs.board_width;
     BoardState.color_wheel = vnode.attrs.color_wheel;
+  },
+  oncreate: function (vnode) {
+    var sb = Snap(vnode.dom);
 
+    // callback hell
+    // request map size
+    // then initialize variable
+    // then request map buffer
+    // then add calback to websocket
     m.request({
       method: "GET",
       url: "/api/map/size",
       extract: function (xhr) { return xhr.responseText }
     })
-      .then(function (result) {
-        let [width, height] = result.split(",");
-
-        BoardState.dimension = {
-          width: width,
-          height: height
-        };
-        BoardState.squareSize = BoardState.size.width / width
-        BoardState.size.height = height * BoardState.squareSize;
-      });
-  },
-  oncreate: function (vnode) {
-    var sb = Snap(vnode.dom);
-
-    m.request({
-      method: "GET",
-      url: "/api/map/buffer",
-      extract: function (xhr) { return xhr.responseText }
-    })
     .then(function (result) {
-      let board = result.split(",");
+      let [width, height] = result.split(",");
 
-      for (let j = 0; j < BoardState.dimension.height; j++) {
-        for (let i = 0; i < BoardState.dimension.width; i++) {
-          let square = sb.rect(i * BoardState.squareSize, j * BoardState.squareSize, BoardState.squareSize, BoardState.squareSize);
-          square.attr({ fill: BoardState.color_wheel[board[i + j * BoardState.dimension.width] % color_wheel.length] });
-          square.attr({ stroke: "#cccccc", strokeWidth: 1 });
-          BoardState.square_list.push(square);
+      BoardState.dimension = {
+        width: width,
+        height: height
+      };
+      BoardState.squareSize = BoardState.size.width / width
+      BoardState.size.height = height * BoardState.squareSize;
+
+      m.request({
+        method: "GET",
+        url: "/api/map/buffer",
+        extract: function (xhr) { return xhr.responseText }
+      })
+      .then(function (result) {
+        let board = result.split(",");
+        for (let j = 0; j < BoardState.dimension.height; j++) {
+          for (let i = 0; i < BoardState.dimension.width; i++) {
+            let square = sb.rect(i * BoardState.squareSize, j * BoardState.squareSize, BoardState.squareSize, BoardState.squareSize);
+            square.attr({ fill: BoardState.color_wheel[board[i + j * BoardState.dimension.width] % color_wheel.length] });
+            square.attr({ stroke: "#cccccc", strokeWidth: 1 });
+            BoardState.square_list.push(square);
+          }
         }
-      }
-    }).then(function () {
-
+        console.log("finish create board");
         socket.onmessage = (message) => {
           let board = message.data.split(",");
           for (let j = 0; j < BoardState.dimension.height; j++) {
@@ -89,7 +91,8 @@ var Board = {
           }
           console.log("update from ws done");
         };
-      });
+      })
+    });
   },
   view: function (vnode) {
     return m("svg#board", { width: BoardState.size.width, height: BoardState.size.height });
